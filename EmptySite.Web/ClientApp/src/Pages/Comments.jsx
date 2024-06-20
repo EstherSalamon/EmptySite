e3444dae5fef512a7ad00d0713f1adfdb01b86a5
Esther Salamon <esther386386@gmail.com>
2024-06-19 22:54:41 -0400

    Add project files.

---
+++ EmptySite.Web/ClientApp/src/Pages/Comments.jsx
@@ -0,0 +1,31 @@
+﻿
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css';
+
+const Comments = () => {
+
+    const [comment, setComment] = useState('');
+    const [message, setMessage] = useState('');
+
+    const onButtonClick = () => {
+        setMessage("I'm sorry, but I'm not paying money to give this dumb little site a backend! If you feel that you were cheated out of a comment that was rightfully yours, call me to relate it in person and get one (1) FREE reaction! (or two, if the comment is good enough) *terms and conditions may apply");
+        setComment('');
+    }
+
+    return (
+        <div style={{ marginTop: 80 }}>
+            <h1>Here, I decided to be kind enough and give this site some functionality! Do you have any suggestions/comments?
+                You can put them up here, for everyone to see. How awesome!</h1>
+            <hr />
+            <textarea className='form-control mb-2' rows="3" placeholder='Write your comments here' value={comment} onChange={e => setComment(e.target.value) } />
+            <br />
+            <button className='btn btn-info' onClick={onButtonClick}>Submit</button>
+            <br />
+            {!!message && <h3>{message}</h3> }
+        </div>
+    )
+};
+
+
+export default Comments;
---
+++ EmptySite.Web/ClientApp/src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css';
+
+const Home = () => {
+
+    return (
+        <div className="app-container">
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <h1>Welcome to my Emptiness site!</h1>
+                <p>This is an empty site. For all of those who wanted to see what a site looks like,
+                    this is a visual aid. It is called an emptiness site because there is nothing of interest here.
+                    That being the case because I do not have any ideas of what to put on to this
+                    amazing, inspirational site. If you do not like the awesomeness of emptiness, too bad.
+                    But if you have an idea of what I can put on here...</p>
+                <a href='/comments' className='btn btn-outline-info'>Give me a suggestion!</a>
+            </div>
+        </div>
+    )
+};
+
+
+export default Home;
